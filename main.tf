82f0bc44de974993edb279538f133c066a834685
beatrizcl757git config --global user.email beatrizcl757@gmail.com <cloudshell-user@ip-10-130-24-104.us-east-2.compute.internal>
2024-10-20 15:52:06 +0000

    Adicionando projeto Terraform inicial

---
+++ main.tf
@@ -0,0 +1,50 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+variable "projeto" {
+  description = "Nome do projeto"
+  type        = string
+  default     = "VExpenses"
+}
+
+variable "candidato" {
+  description = "Nome do candidato"
+  type        = string
+  default     = "SeuNome"
+}
+
+resource "tls_private_key" "ec2_key" {
+  algorithm = "RSA"
+  rsa_bits  = 2048
+}
+
+resource "aws_key_pair" "ec2_key_pair" {
+  key_name   = "${var.projeto}-${var.candidato}-key"
+  public_key = tls_private_key.ec2_key.public_key_openssh
+
+  depends_on = [tls_private_key.ec2_key]
+}
+
+resource "aws_vpc" "main_vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name        = "${var.projeto}-${var.candidato}-vpc"
+    Environment = "development"  # ou "production"
+    Owner       = var.candidato
+  }
+}
+
+resource "aws_subnet" "main_subnet" {
+  vpc_id            = aws_vpc.main_vpc.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"  # Altere conforme necessário
+
+  tags = {
+    Name = "${var.projeto}-${var.candidato}-subnet"
+  }
+}
+
